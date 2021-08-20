41f0de59ae15e99348fdc85e29638b9b27de9b6e
KoFeeBriAN <tuwanon.coffee@gmail.com>
2021-08-20 19:00:27 +0900

    add tsumugi images and style

--- app/app.js
+++ app/app.js
@@ -1,79 +1,144 @@
 import React, { useEffect, useState, useReducer } from "react";
 import "tailwindcss/tailwind.css";
-import withDom, { withDomGen, useTargetDom, runContentScript } from "./utils/chrome/access";
-import { executeSpeedBinbReaderScript, executeNicoDougaScript, executeComicWalkerScript } from "./handlers/entries/entry";
+import withDom, {
+  withDomGen,
+  useTargetDom,
+  runContentScript,
+} from "./utils/chrome/access";
+import {
+  executeSpeedBnbReaderScript,
+  executeNicoDougaScript,
+} from "./handlers/entries/entry";
 import { unpackReducer, timeout } from "./utils/utils";
 
+import idealImage from "../static/assets/ideal.png";
+import processingImage from "../static/assets/processing.png";
+import successImage from "../static/assets/success.png";
+import bgIamge from "../static/assets/bg.png";
+
 export default function App() {
-	const [meta, setMeta] = useReducer(unpackReducer, {
-		chapter: 0,
-		title: "KoibitoMuriMuri",
-		filenamePrefix: `KoibitoMuriMuri Ch.0`,
-	});
+  const [meta, setMeta] = useReducer(unpackReducer, {
+    chapter: 0,
+    title: "KoibitoMuriMuri",
+    filenamePrefix: `KoibitoMuriMuri Ch.0`,
+  });
 
-	useEffect(() => {
-		// setMeta({ filenamePrefix: `${meta.title} Ch.${meta.chapter}` });
-	}, [meta.chapter, meta.title]);
+  useEffect(() => {
+    // TODO: Uncomment this line in production mode
+    // setMeta({ filenamePrefix: `${meta.title} Ch.${meta.chapter}` });
+  }, [meta.chapter, meta.title]);
 
-	const [pageRanges, setPageRanges] = useReducer(unpackReducer, {
-		startPage: 0,
-		endPage: 60,
-	});
+  const [pageRanges, setPageRanges] = useReducer(unpackReducer, {
+    startPage: 0,
+    endPage: 60,
+  });
 
-	const mainTest = async () => {
-		console.log("mainTested");
-		// runContentScript();
-		// messages.send({
-		//     greeting: "greeting from app.js",
-		//     data: "",
-		// });
-	};
+  const mainTest = async () => {
+    console.log("mainTested");
+    // runContentScript();
+    // messages.send({
+    //     greeting: "greeting from app.js",
+    //     data: "",
+    // });
+  };
 
-	return (
-		<>
-			<div className="flex flex-col p-4 h-full">
-				<h1 className="leading-3 font-bold">Manga Extractor</h1>
+  const [processState, setProcessState] = useState("success");
+  /// Testing
+  //   const states = ["ideal", "processing", "success"];
+  //   useEffect(() => {
+  //     let i = 0;
+  //     let interval = setInterval(() => {
+  //       setProcessState(states[i]);
+  //       i = (i + 1) % 3;
+  //     }, 3000);
+  //     return () => clearInterval(interval);
+  //   }, []);
 
-				<label htmlFor="reader" className="text-lg font-bold">
-					Reader
-				</label>
+  const ディッブ = (props) => {
+    return <div {...props}>{props.children}</div>;
+  };
+  const つむぎ = ({ className, state }) => {
+    return state === "success" ? (
+      <img src={successImage} className={className}></img>
+    ) : state === "processing" ? (
+      <img src={processingImage} className={className}></img>
+    ) : (
+      <img src={idealImage} className={className}></img>
+    );
+  };
 
-				<div id="reader-select" className="relative">
-					<input
-						name="reader"
-						type="text"
-						placeholder="ABC Reader"
-						className="self-center p-1 my-2 w-4/5 rounded-md border-2 border-red-500 focus:ring-2 focus:ring-red-400 text-gray-500 outline-none"
-					/>
-					<div className="relative">
-						<button className="cursor-pointer flex items-center outline-none focus:outline-none">
-							Reader A
-						</button>
-					</div>
-				</div>
+  return (
+    <>
+      <ディッブ
+        className="relative overflow-hidden h-full"
+        style={{
+          backgroundImage: `url(${bgIamge})`,
+          backgroundSize: `cover`,
+          backgroundPosition: `90% 40%`,
+        }}
+      >
+        <div className="relative h-full backdrop-filter backdrop-blur-md backdrop-brightness-70">
+          <section className="flex flex-col p-4">
+            <h1 className="mb-4 text-3xl text-white text-stroke paint-sfm-paintOrder text-stroke-black font-bold">
+              Manga Extractor
+            </h1>
+            <ディッブ
+              id="reader-select"
+              className="relative grid grid-cols-2 grid-rows-2 gap-2"
+            >
+              <div className="row-start-1 row-end-2 col-start-1 col-end-3 flex flex-col">
+                <label
+                  htmlFor="reader"
+                  className="text-xl text-fill-black text-stroke text-stroke-white font-bold"
+                >
+                  Reader
+                </label>
+                <input name="reader" type="text" placeholder="ABC Reader" />
+              </div>
+              <div className="row-start-2 row-end-3 col-start-1 col-end-2 flex flex-col">
+                <label
+                  htmlFor="zip-name"
+                  className="text-xl text-fill-black text-stroke font-bold"
+                >
+                  Zip Name
+                </label>
+                <input name="zip-name" type="text" />
+              </div>
+              <div className="row-start-2 row-end-3 col-start-2 col-end-3 flex flex-col">
+                <label
+                  htmlFor="page-name"
+                  className="text-xl text-fill-black text-stroke font-bold"
+                >
+                  Page Name
+                </label>
+                <input name="page-name" type="text" />
+              </div>
+            </ディッブ>
+          </section>
 
-				<label htmlFor="zip-name">Zip Name</label>
-				<input name="zip-name" type="text" />
-				<label htmlFor="page-name">Page Name</label>
-				<input name="page-name" type="text" />
-
-				<div className="flex justify-center">
-					<button
-						type="button"
-						className="rounded-md px-4 py-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white"
-					>
-						Reset
-					</button>
-					<div className="p-2"></div>
-					<button
-						type="button"
-						className="rounded-md px-4 py-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white"
-					>
-						Extract!
-					</button>
-				</div>
-			</div>
-			{/* <div className="container">
+          <section className="relative mt-4 min-h-full">
+            <div className="relative -left-14 w-11/12">
+              <つむぎ
+                className="filter drop-shadow-ideal transform scale-90 -translate-y-8"
+                state={processState}
+              />
+            </div>
+            <p className="absolute p-2 w-1/3 top-20 right-6 border-2 border-red-500">
+              むきゅ。。。いらっしゃいませ！。
+            </p>
+            <div className="absolute top-1/3 right-8">
+              <button
+                type="button"
+                className="rounded-md px-4 py-8 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white"
+              >
+                開
+                <br />始
+              </button>
+            </div>
+          </section>
+        </div>
+      </ディッブ>
+      {/* <div className="container">
 				<h1>MEextention</h1>
 
 				<button
@@ -103,6 +168,6 @@ export default function App() {
 					executeSpeedBnbReaderScript
 				</button>
 			</div> */}
-		</>
-	);
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-	purge: ["./build/index.html"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {},
-	plugins: [],
+  purge: ["./build/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    textFillColor: theme => theme('borderColor'),
+    textStrokeColor: theme => theme('borderColor'),
+    textStrokeWidth: theme => theme('borderWidth'),
+    paintOrder: {
+      'fsm': { paintOrder: 'fill stroke markers' },
+      'fms': { paintOrder: 'fill markers stroke' },
+      'sfm': { paintOrder: 'stroke fill markers' },
+      'smf': { paintOrder: 'stroke markers fill' },
+      'mfs': { paintOrder: 'markers fill stroke' },
+      'msf': { paintOrder: 'markers stroke fill' },
+    },
+    extend: {
+      brightness: {
+        70: ".7",
+      },
+      dropShadow: {
+        ideal: "0 0 20px #EABF80",
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require("tailwindcss-text-fill-stroke")(),
+  ],
 };
